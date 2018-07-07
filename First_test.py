1003b942bfcf2aa83a7c40643c1c894096724272
AlanShmyga <alexey.shmyga@gmail.com>
2018-07-07 15:14:17 +0300

    Fist test added

---
+++ First_test.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ExpectedConditions
+from selenium.webdriver.common.keys import Keys
+
+
+@pytest.fixture()
+def driver(request):
+    web_driver = webdriver.Chrome()
+    web_driver.implicitly_wait(10)
+    request.addfinalizer(web_driver.quit)
+    return web_driver
+
+
+def test_example(driver):
+    driver.get("http://www.google.com/")
+    driver.find_element_by_name("q").send_keys("AlanShmyga")
+    driver.find_element_by_name("q").send_keys(Keys.ENTER)
+    WebDriverWait(driver, 10).until(ExpectedConditions.title_is("AlanShmyga - Поиск в Google"))
